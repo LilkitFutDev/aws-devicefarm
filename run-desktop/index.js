dd837699a3d0fd66b6e4b1bf2d3548b2a27fb40b
LilkitFutDev <Lilkitfutdev@gmail.com>
2022-08-13 15:10:06 +0300

    desktop run 0.1

--- run-desktop/index.js
+++ run-desktop/index.js
@@ -4,35 +4,26 @@ const devicefarm = require("../lib/aws-devicefarm.js");
 var params = {};
 var params_test = {};
 
-params.appArn = core.getInput('app_arn');
 // alternative selection method would be via filters in
 // deviceSelectionConfiguration - as pool arn is way easier
 // stick with that for now
 params.projectArn = core.getInput('project_arn');
 params.name = core.getInput('name');
+params.expiresInSeconds = core.getInput('expires_in_seconds');
 
 params_test.type = core.getInput('test_type');
 params_test.testPackageArn = core.getInput('test_package_arn');
 params_test.testSpecArn = core.getInput('test_spec_arn');
 
 if (!devicefarm.validateParameters(params,
-    {appArn: 'app_arn',
-        name: '',
-        devicePoolArn: 'device_pool_arn',
-        projectArn: 'project_arn'})) {
-    return;
-}
-
-if (!devicefarm.validateParameters(params_test,
-    {type: 'test_type',
-        testPackageArn: 'test_package_arn',
-        testSpecArn: 'test_spec_arn'})) {
+    {projectArn: 'project_arn',
+             expiresInSeconds: 'expires_in_seconds'})) {
     return;
 }
 
 params.test = params_test;
 
-devicefarm.scheduleRun(params)
+devicefarm.createTestGridUrl(params)
     .then(data => {
         core.setOutput("data", data);
         core.setOutput("arn", data.run.arn);
